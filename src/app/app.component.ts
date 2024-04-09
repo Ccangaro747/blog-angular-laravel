a4c74e81bfa937b93e8ae034378fcec5c446673e
Camilo Cangaro <camilocangaro@gmail.com>
2024-04-09 13:53:38 -0300

    Actualizar barra de navegación y menú desplegable en la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // Selector CSS que identifica este componente en el HTML
+  templateUrl: './app.component.html', // Ubicación del archivo HTML que contiene la plantilla del componente
+  styleUrls: ['./app.component.css'] // Ubicación del archivo CSS que contiene los estilos del componente
 })
 export class AppComponent {
-  isMenuOpen = false;
+  isMenuOpen = false; // Propiedad que controla si el menú está abierto o cerrado, inicializada en false
+  isUserMenuOpen = false; // Propiedad que controla si el menú de usuario está abierto o cerrado, inicializada en false
 
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
+  toggleMenu() { // Método para alternar el estado del menú
+    this.isMenuOpen = !this.isMenuOpen; // Invierte el valor de isMenuOpen
+  }
+
+  toggleUserMenu() { // Método para alternar el estado del menú de usuario
+    this.isUserMenuOpen = !this.isUserMenuOpen; // Invierte el valor de isUserMenuOpen
   }
 }
