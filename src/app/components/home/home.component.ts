c93c0a1ffedd0766bfe1a1f5fe945e4e3aef7ca3
Camilo Cangaro <camilocangaro@gmail.com>
2024-04-23 12:59:47 -0300

    Mostrar botones a los usuarios identificados

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,24 +2,30 @@ import { Component } from '@angular/core';
 import { Post } from '../../models/post';
 import { PostService } from '../../services/post.service';
 import { global } from '../../services/global';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  providers: [PostService]
+  providers: [PostService, UserService]
 })
 export class HomeComponent {
   public page_title: string;
   public url: string;
   public posts: Array<Post>;
+  public identity: any;
+  public token: any;
 
   constructor(
-    private _postService: PostService
+    private _postService: PostService,
+    private _userService: UserService
   ){
     this.page_title = "Bienvenido";
     this.url = global.url;
     this.posts = [];
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
   ngOnInit(){
