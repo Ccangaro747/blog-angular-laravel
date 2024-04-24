884cd40726c4773ab1042029cfcfd27e973dddc5
Camilo Cangaro <camilocangaro@gmail.com>
2024-04-24 10:52:34 -0300

    Sacar información de una entrada

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from './../../models/post';
+import { PostService } from './../../services/post.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
-  styleUrl: './post-detail.component.css'
+  styleUrls: ['./post-detail.component.css'],
+  providers: [PostService]
 })
-export class PostDetailComponent {
-  constructor() { }
+export class PostDetailComponent implements OnInit {
+  public post: Post;
 
-  ngOnInit(): void {
+  constructor(
+    private _postService: PostService,
+    private _route: ActivatedRoute,
+    private _router: Router,
+  ) {
+    // Inicializa la propiedad post en el constructor
+    this.post = new Post(0, 0, 0, '', '', '', null);
   }
 
+  ngOnInit(): void {
+    this.getPost();
+  }
+
+  getPost(): void {
+    // Sacar el id del post de la url
+    this._route.params.subscribe(params => {
+      let id = +params['id'];
+
+      // Aquí puedes hacer la petición AJAX para obtener los datos del post
+      this._postService.getPost(id).subscribe(
+        response => {
+          if (response.status == 'success') {
+            this.post = response.post;
+            console.log(this.post);
+          } else {
+            this._router.navigate(['/inicio']);
+          }
+        },
+        error => {
+          console.log(<any>error);
+          this._router.navigate(['/inicio']);
+        }
+
+      );
+      // usando el id obtenido de los parámetros de la ruta.
+    });
+  }
 }
