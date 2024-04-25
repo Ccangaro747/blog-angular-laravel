2c72cb8daca01a102d4f2b7f1c040815a4202f70
Camilo Cangaro <camilocangaro@gmail.com>
2024-04-25 19:42:27 -0300

    Añadida funcionalidad de mostrar contenido HTML sanitizado en el componente PostDetailComponent

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -12,16 +12,17 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 })
 export class PostDetailComponent implements OnInit {
   public post: Post;
-
+  public sanitizedContent: SafeHtml = [];
   constructor(
     private _postService: PostService,
     private _route: ActivatedRoute,
     private _router: Router,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private SafeHtml: DomSanitizer,
   ) {
     // Inicializa la propiedad post en el constructor
     this.post = new Post(0, 0, 0, '', '', '', null);
-    
+
   }
 
   ngOnInit(): void {
@@ -38,6 +39,8 @@ export class PostDetailComponent implements OnInit {
         response => {
           if (response.status == 'success') {
             this.post = response.post;
+            // Sanitizar el contenido HTML antes de asignarlo a la variable
+            this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(this.post.content);
             console.log(this.post);
           } else {
             this._router.navigate(['/inicio']);
